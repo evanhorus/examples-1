54f4372d3eec8960ea255eb2217299da4f831cb6
evanhorus@gmail.com <evanhorus@gmail.com>
2022-05-11 10:10:06 +0300

    add new styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import './App.css'
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  background-color: yellow;
+  display: flex;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  align-content: stretch;
+  height: 100%;
+`;
 
 export const App = () => {
     return (
-        <>
-            <div className='myClass'>Hello</div>
-            <div style={{
-                backgroundColor: 'green'
-            }}>Hello
+        <Container>
+            <div className='myClass'>
+                Hello
             </div>
-        </>)
+            <div style={{backgroundColor: 'blue', flex: 1}}>
+                Hello
+            </div>
+            <StyledDiv>
+                Hello
+            </StyledDiv>
+        </Container>
+    );
 }
