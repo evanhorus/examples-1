9bf5d77c0830983fb63532716f371914bbc3d834
evanhorus@gmail.com <evanhorus@gmail.com>
2022-05-11 20:24:41 +0300

    remove strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css'
 import styled from "styled-components";
+import {MyComponent2, MyComponents} from "./components/MyComponent";
+import {ExamplePage} from "./pages/ExamplePage";
 
 const StyledDiv = styled.div`
   background-color: yellow;
@@ -14,15 +16,26 @@ const Container = styled.div`
   align-items: stretch;
   align-content: stretch;
   height: 100%;
+  background-color: red;
 `;
+
+const ModifiedContainer = styled(Container)`
+  background-color: aquamarine;
+`
 
 export const App = () => {
     return (
         <Container>
-            <div className='myClass'>
-                Hello
-            </div>
+            <ModifiedContainer>
+                <div className='myClass'>
+                    Hello
+                </div>
+            </ModifiedContainer>
             <div style={{backgroundColor: 'blue', flex: 1}}>
+                <ExamplePage>
+                    <MyComponents myProp={42}/>
+                    <MyComponent2/>
+                </ExamplePage>
                 Hello
             </div>
             <StyledDiv>
